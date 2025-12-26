571f713c6c572810f0af4cbb5f45d33f2d96837f
carsan-dev <crolyx16@gmail.com>
2025-12-26 17:12:12 +0100

    Añadidos los nuevos campos de los alumnos

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,15 @@
+export type TipoAsistencia = 'completo' | 'medio' | 'dias';
+
 export type Alumno = {
   nombre: string;
   cantidad: number;
+  activo: boolean;
+  tipoAsistencia: TipoAsistencia;
+  diasAsistencia?: number; // Solo se usa cuando tipoAsistencia es 'dias'
 };
 
 export type RootStackParamList = {
+  Login: undefined;
   Home: undefined;
   Pdf: { alumnos: Alumno[] };
 };
